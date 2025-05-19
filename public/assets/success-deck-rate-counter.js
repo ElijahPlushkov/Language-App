f5e1e2ece0008a28f651a6f9a6d89447544d88c9
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-05-19 22:02:45 +0300

    Add files via upload

--- public/assets/success-deck-rate-counter.js
+++ public/assets/success-deck-rate-counter.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-     
+document.addEventListener("DOMContentLoaded", function () {
+    let scoreCounter = 0;
+    const finalScoreDisplay = document.querySelector(".final-score-display");
+    const cards = document.querySelectorAll(".card"); 
+
+    function calculateSuccessRate() {
+        const numberOfCards = cards.length;
+        const successRate = (scoreCounter / numberOfCards) * 100;
+        const successRateDisplay = document.querySelector(".success-rate-display");
+        successRateDisplay.textContent = successRate.toFixed(2) + "%";
+    }
+
+    document.querySelectorAll(".increase-success-rate").forEach(btn => {
+        btn.addEventListener("click", () => {
+            scoreCounter++;
+            finalScoreDisplay.textContent = scoreCounter;
+            calculateSuccessRate();
+        });
+    });
 });
-
-function calculateSuccessRate() {
-    
-}
-
-function increaseSuccessRate() {
-    
-}
-
-function decreaseSuccessRate() {
-    
-}
