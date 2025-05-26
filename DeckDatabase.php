18cfdc5210090548804ebd489690a9778ee61a2d
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-05-26 15:18:11 +0300

    code refactoring

---
+++ DeckDatabase.php
@@ -0,0 +1,8 @@
+<?php
+
+function getAllDecks(PDO $pdo, int $userId): array {
+    $sql = $pdo->prepare("SELECT * FROM `decks` WHERE user_id = ?");
+    $sql->execute([$userId]);
+    $decks = $sql->fetchAll(PDO::FETCH_ASSOC);
+    return $decks;
+}
