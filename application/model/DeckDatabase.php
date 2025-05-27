d0c4dbf2b7f6136e35c21d49d5644f13c46c3830
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-05-27 22:16:49 +0300

    Add files via upload

--- application/model/DeckDatabase.php
+++ application/model/DeckDatabase.php
@@ -4,4 +4,5 @@ function getAllDecks(PDO $pdo, int $userId): array {
     $sql = $pdo->prepare("SELECT * FROM `decks` WHERE user_id = ?");
     $sql->execute([$userId]);
     $decks = $sql->fetchAll(PDO::FETCH_ASSOC);
+    return $decks;
 }
