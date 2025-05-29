fcb6eca3bd11337c4c9fc9776c4e4535d0864d95
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-05-29 12:58:50 +0300

    new functions
    added 2 functions for processing ids

--- core/functions.php
+++ core/functions.php
@@ -5,3 +5,20 @@ function abort($code = 404) {
     require VIEWS . "/errors/{$code}.tpl.php";
     die;
 }
+
+//the function returns either an integer or null --> ?int (int or null)
+function getUserId(array $session): ?int {
+    if (!isset($session['user_id']) || $session['user_id'] === null) {
+        return null;
+    }
+    $userId = (int) $session['user_id'];
+    return $userId;
+}
+
+function getDeckId(array $get): ?int {
+    if (!isset($get['deck_id'])) {
+        return null;
+    }
+    $deckId = (int)$get['deck_id'];
+    return $deckId;
+}
